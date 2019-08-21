2b210ce9d937b41d7023604527435dabca8da760
Tristam Archer <aut.archer.tristam@gmail.com>
2019-08-21 19:08:36 +1200

    No mans Sky Project Submit

---
+++ No Man's Land/Assets/Our Scripts/Player_Network.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_Network : NetworkBehaviour
+{
+    public GameObject firstPersonCharacter;
+    public GameObject[] characterModel;
+    // Start is called before the first frame update
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<FirstPersonController>().enabled = true;
+        firstPersonCharacter.SetActive(true);
+
+        foreach(GameObject obj in characterModel)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
