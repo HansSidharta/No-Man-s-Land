ada1be8c5d1c49b09b929be4341db57e3c61d76b
Hans <hans.sidharta@yahoo.com>
2019-09-06 17:10:48 +1200

    added my part
    Added:
    -BigExplosion.prefab
    -Bounce.physicMaterial
    -Grenade.cs
    -Sphere.prefab
    -Throw.cs

---
+++ No Man's Land/Assets/Our Scripts/Grenade.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    public float blasteradius = 1f;
+    float countdown;
+    public float force = 700f;
+    bool hasExploded = false;
+    public GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Collider[] coll = Physics.OverlapSphere(transform.position,blasteradius);
+        foreach(Collider nearbyObject in coll)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position,blasteradius);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ No Man's Land/Assets/Our Scripts/Throw.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    private Vector2 initialFingerPosition;
+    private Vector2 finalFingerPosition;
+    public float throwForce;
+    public GameObject grenadePrefab;
+
+    [SerializeField]
+    private float minDistanceForSwipe = 5f;
+    private bool detectSwipeAfterRelease = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                initialFingerPosition = touch.position;
+            }
+
+            //if (!detectSwipeAfterRelease && touch.phase == TouchPhase.Moved)
+            //{
+            //    finalFingerPosition = touch.position;
+            //    DetectionSwipe();
+            //}
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                finalFingerPosition = touch.position;
+                DetectionSwipe();
+            }
+        }
+    }
+
+    void throwGrenade()
+    {
+        throwForcecalc(VerticalMovementDistance());
+        GameObject gr = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = gr.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward*throwForce, ForceMode.VelocityChange);
+    }
+
+    private void DetectionSwipe()
+    {
+        if(SwipeDistanceCheckMet())
+        {
+            if(isSwipeStraight())
+            {
+                throwGrenade();
+            }
+        }
+    }
+
+    private bool isSwipeStraight()
+    {
+        return VerticalMovementDistance() > HorizontalMovementDistance();
+    }
+
+    private bool SwipeDistanceCheckMet()
+    {
+        return VerticalMovementDistance() > minDistanceForSwipe;
+    }
+
+    private float VerticalMovementDistance()
+    {
+        return Mathf.Abs(finalFingerPosition.y - initialFingerPosition.y);
+    }
+
+    private float HorizontalMovementDistance()
+    {
+        return Mathf.Abs(finalFingerPosition.x - initialFingerPosition.x);
+    }
+
+    void throwForcecalc(float swipeDist)
+    {
+        throwForce = 0.09f * swipeDist;
+    }
+}
