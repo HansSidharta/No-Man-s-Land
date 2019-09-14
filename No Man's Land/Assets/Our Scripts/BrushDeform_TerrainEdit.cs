9476961db9a4adefd262f9edfbd88100d9fc7349
Tristam Archer <aut.archer.tristam@gmail.com>
2019-09-15 07:44:34 +1200

    Update terrain code

--- No Man's Land/Assets/Our Scripts/BrushDeform_TerrainEdit.cs
+++ No Man's Land/Assets/Our Scripts/BrushDeform_TerrainEdit.cs
@@ -12,7 +12,9 @@ public class BrushDeform_TerrainEdit : MonoBehaviour
 
     [SerializeField] private AnimationCurve forceOverDistance = AnimationCurve.Constant(0, 1, 1);
 
-    [SerializeField] private World world;
+    private World world;
+
+    private GameObject temp;
     [SerializeField] private Transform attachedMeshObject;
     Chunk[] _initChunks;
     Mesh mesh;
@@ -22,38 +24,63 @@ public class BrushDeform_TerrainEdit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Initialise Cube chunk
         _initChunks = new Chunk[8];
-        mesh = attachedMeshObject.GetComponent<MeshFilter>().mesh;       
+
+        // Get mesh data from attached object
+        mesh = attachedMeshObject.GetComponent<MeshFilter>().mesh;   
+        // Get vertex data from attached mesh    
         vertices = mesh.vertices;
+
+        // Search entire scene to find Gameworld
+        temp = GameObject.Find("World_Test");
+
+        // If found set to world
+        if (temp != null)
+        {
+            world = temp.GetComponent<World>();
+        }
     }
 
     void Update()
     {
+        // Update run every frame
+        // Set up co-routine to save perf (~40-50% saved)
+        // Coroutine runs on main thread. Run non-critical code
+        // Start Coroutine
         StartCoroutine("UpdateFrame");
     }
 
     IEnumerator UpdateFrame()
     {
+        // Loop through each vertex in verticies
         foreach (Vector3 vert in vertices)
         {
+            // Convert vertex from local to world location
             worldPoint = transform.TransformPoint(vert);
+
+            // Pass world point through to terrain editor
             EditTerrain(worldPoint, addTerrain, force, range);
         }
 
+        // Yeild to the main thread after given period
         yield return new WaitForSeconds(.1f);
     }
 
     private void EditTerrain(Vector3 point, bool addTerrain, float force, float range)
     {
+        // strength modifier
         int buildModifier = addTerrain ? 1 : -1;
 
+        // round 3D floats to ints
         int hitX = point.x.Round();
         int hitY = point.y.Round();
         int hitZ = point.z.Round();
 
-
+        // Round up float to int
         int intRange = range.Ceil();
 
+        // loop through all points in world
         for (int x = -intRange; x <= intRange; x++)
         {
             for (int y = -intRange; y <= intRange; y++)
@@ -64,6 +91,8 @@ public class BrushDeform_TerrainEdit : MonoBehaviour
                     int offsetY = hitY - y;
                     int offsetZ = hitZ - z;
 
+
+                    // Check if given points are within world bounds
                     if (!world.IsPointInsideWorld(offsetX, offsetY, offsetZ))
                         continue;
 
@@ -71,12 +100,14 @@ public class BrushDeform_TerrainEdit : MonoBehaviour
                     if (!(distance <= range)) continue;
 
                     float modificationAmount = force / distance * forceOverDistance.Evaluate(1 - distance.Map(0, force, 0, 1)) * buildModifier;
-
+                    
+                    // Generate new world data
                     float oldDensity = world.GetDensity(offsetX, offsetY, offsetZ);
                     float newDensity = oldDensity - modificationAmount;
 
                     newDensity = newDensity.Clamp01();
 
+                    // Set new world data
                     world.SetDensity(newDensity, offsetX, offsetY, offsetZ, true, _initChunks);
                 }
             }
--- No Man's Land/Assets/Our Scripts/SpotDeform_TerrainEdit_ver2.cs
+++ No Man's Land/Assets/Our Scripts/SpotDeform_TerrainEdit_ver2.cs
@@ -12,8 +12,7 @@ public class SpotDeform_TerrainEdit_ver2 : MonoBehaviour
 
     [SerializeField] private AnimationCurve forceOverDistance = AnimationCurve.Constant(0, 1, 1);
     private World world;
-    private GameObject temp;
-    
+    private GameObject temp;    
     [SerializeField] private Transform attachedMeshObject;
     private Vector3[] vertices;
     Mesh mesh;
@@ -23,38 +22,54 @@ public class SpotDeform_TerrainEdit_ver2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mesh = attachedMeshObject.GetComponent<MeshFilter>().mesh;
+        // Initialise Cube Chunk
         _initChunks = new Chunk[8];
+
+        // Get mesh data from attached Object
+        mesh = attachedMeshObject.GetComponent<MeshFilter>().mesh;
+        
+        // Get vertex data from Mesh
         vertices = mesh.vertices;
         
+        // Find instance of Gameworld from scene
         temp = GameObject.Find("World_Test");
+
+        // If game world found, assign to world
         if (temp != null)
         {
             world = temp.GetComponent<World>();
         }
 
+        // Update frame
         UpdateFrame();
     }
 
     void UpdateFrame()
     {
+        // loop through each vertex to get 3D point
         foreach (Vector3 vert in vertices)
         {
+            // Convert 3D local point to world point
             worldPoint = transform.TransformPoint(vert);
+            // Pass point to terrain editor
             EditTerrain(worldPoint, addTerrain, force, range);
         }
     }
 
     private void EditTerrain(Vector3 point, bool addTerrain, float force, float range)
     {
+        // strength modifier
         int buildModifier = addTerrain ? 1 : -1;
 
+        // round 3D floats to ints
         int hitX = point.x.Round();
         int hitY = point.y.Round();
         int hitZ = point.z.Round();
 
+        // Round up float to int
         int intRange = range.Ceil();
 
+        // loop through all points in world
         for (int x = -intRange; x <= intRange; x++)
         {
             for (int y = -intRange; y <= intRange; y++)
@@ -65,6 +80,8 @@ public class SpotDeform_TerrainEdit_ver2 : MonoBehaviour
                     int offsetY = hitY - y;
                     int offsetZ = hitZ - z;
 
+
+                    // Check if given points are within world bounds
                     if (!world.IsPointInsideWorld(offsetX, offsetY, offsetZ))
                         continue;
 
@@ -72,12 +89,14 @@ public class SpotDeform_TerrainEdit_ver2 : MonoBehaviour
                     if (!(distance <= range)) continue;
 
                     float modificationAmount = force / distance * forceOverDistance.Evaluate(1 - distance.Map(0, force, 0, 1)) * buildModifier;
-
+                    
+                    // Generate new world data
                     float oldDensity = world.GetDensity(offsetX, offsetY, offsetZ);
                     float newDensity = oldDensity - modificationAmount;
 
                     newDensity = newDensity.Clamp01();
 
+                    // Set new world data
                     world.SetDensity(newDensity, offsetX, offsetY, offsetZ, true, _initChunks);
                 }
             }
