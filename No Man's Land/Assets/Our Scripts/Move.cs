426bc86fc5310dd98161f324712045dd56e3aed1
Hans <hans.sidharta@yahoo.com>
2019-09-20 11:40:48 +1200

    Added buttons on the screen

---
+++ No Man's Land/Assets/Our Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour
+{
+	public Button leftButton;
+	public Button rightButton;
+	public GameObject chara;
+    private float speed = 0.5f;// movement speed
+    // Start is called before the first frame update
+    void Start()
+    {
+		Button l = leftButton.GetComponent<Button>();// get left button
+        Button r = rightButton.GetComponent<Button>();// get right button
+        l.onClick.AddListener(LeftClick);
+        r.onClick.AddListener(RightClick);
+    }
+
+
+    private void LeftClick()// if left button is clicked
+    {
+        chara.transform.Translate(-speed, 0f, 0f);
+    }
+
+    private void RightClick()// if right button is clicked
+    {
+        chara.transform.Translate(speed, 0f, 0f);
+    }
+}
