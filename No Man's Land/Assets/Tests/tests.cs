067cf607a354964dc1c9b5250f4d65956972de17
Tristam Archer <aut.archer.tristam@gmail.com>
2019-09-20 23:41:55 +1200

    Update to unit test

--- No Man's Land/Assets/Tests/tests.cs
+++ No Man's Land/Assets/Tests/tests.cs
@@ -14,11 +14,9 @@ namespace Tests
         public void _testNullWorldException()
         {  
             // Ensure Test catches when the world is not given in the Editor (A correct 'World name'World must be given to pass)
-            GameObject _spotDeform =  GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-            _spotDeform.AddComponent<SpotDeform_TerrainEdit_ver2>();                        
+            GameObject _spotDeform =  GameObject.CreatePrimitive(PrimitiveType.Cube);                                   
                         
-            Assert.Catch<NullReferenceException>(() => GameObject.Instantiate(_spotDeform));         
+            Assert.Catch<NullReferenceException>(() => _spotDeform.AddComponent<SpotDeform_TerrainEdit_ver2>());         
         }
 
         [Test]
